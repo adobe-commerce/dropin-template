367d330d8db620040af3ffc4f9fdaeac4de3da8a
Carlos A. Cabrera <cacabrer@adobe.com>
2023-05-08 14:26:17 -0500

    favicon

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+export const framework = {
+    name: '@storybook/preact-webpack5',
+    options: {},
+  };
+  
+  export const addons = [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@adobe/elsie/config/storybook/addon',
+    '@adobe/elsie/storybook-stories', // core stories
+    '../storybook-stories', // this project stories
+  ];
+  
+  export const staticDirs = ['./public'];
+  
