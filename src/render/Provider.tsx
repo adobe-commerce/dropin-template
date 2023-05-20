213c7823baa5a318f954ebca3c80200d9f45e09f
Carlos A. Cabrera <cacabrer@adobe.com>
2023-05-20 09:48:26 -0500

    enabel logger

--- src/render/Provider.tsx
+++ src/render/Provider.tsx
@@ -1,9 +1,10 @@
 import { FunctionComponent } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 import { UIProvider } from '@adobe/elsie/components';
 import { Lang } from '@adobe/elsie/i18n';
 import { events } from '@adobe/event-bus';
 
+events.enableLogger(true);
 // Langs
 const langDefinitions = {
   default: require('../i18n/en_US.json'),
@@ -23,7 +24,6 @@ export const Provider: FunctionComponent<CartProviderProps> = ({
     const localeEvent = events.on('locale', (locale: string) => {
       setLang(locale as Lang);
     });
-
     return () => {
       localeEvent.off();
     };
