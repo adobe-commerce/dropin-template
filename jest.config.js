ec089981eaf3920b0054d390aee1ee39f6530989
Carlos A. Cabrera <cacabrer@adobe.com>
2023-05-20 17:04:12 -0500

    config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,3 @@
-require('dotenv').config({ path: '.env.local' });
-
 const baseConfig = require('@adobe/elsie/config/jest');
 
-module.exports = {
-  ...baseConfig,
-
-  moduleNameMapper: { ...baseConfig.moduleNameMapper },
-
-  coverageThreshold: {
-    global: {
-      branches: 68,
-      functions: 63,
-      lines: 70,
-      statements: 70,
-    },
-  },
-};
+module.exports = { ...baseConfig };
