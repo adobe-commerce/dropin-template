04c58f921fc2fea0566f8b177581e167a2f04cbb
Carlos Cabrera <cacabrer@adobe.com>
2023-05-23 11:33:33 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ const baseConfig = require('@adobe/elsie/config/jest');
 
 module.exports = {
   ...baseConfig,
+  
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
