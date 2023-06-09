63fe16659f014492ebad06493bbf47c51fbd4def
Carlos A. Cabrera <cacabrer@adobe.com>
2023-06-09 11:37:23 -0500

    fetch-graphql per instance

--- src/api/initialize/initialize.ts
+++ src/api/initialize/initialize.ts
@@ -1,13 +1,11 @@
-import { Config, Initializer } from '@adobe/elsie/lib';
+import { Initializer } from '@adobe/elsie/lib';
 // import { events } from '@adobe/event-bus';
 
 type ConfigProps = {};
 
-export const config = new Config<ConfigProps>({});
-
-export const initialize = new Initializer({
-  init: async (_config?: ConfigProps) => {
-    if (_config) config.setConfig(_config);
+export const initialize = new Initializer<ConfigProps>({
+  init: async (config) => {
+    //
   },
 
   listeners: () => [
@@ -16,3 +14,5 @@ export const initialize = new Initializer({
     // }),
   ],
 });
+
+export const config = initialize.config;
