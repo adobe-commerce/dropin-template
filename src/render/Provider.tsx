6d57d5678a51f7ab3dbc3ebd2bcfd54d0499bc60
Miquel Angel Coca Piza <cocapiza@Miquels-MacBook-Pro.local>
2026-03-17 16:17:09 +0100

    feat: enhance language definition merging in Provider

--- src/render/Provider.tsx
+++ src/render/Provider.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'preact/hooks';
 import { UIProvider } from '@adobe-commerce/elsie/components';
 import { Lang } from '@adobe-commerce/elsie/i18n';
 import { events } from '@adobe-commerce/event-bus';
+import { deepmerge } from '@adobe-commerce/elsie/lib';
+import { config } from '@/dropin-name/api';
 
 import en_US from '../i18n/en_US.json';
 
@@ -11,11 +13,11 @@ const langDefinitions = {
   default: en_US,
 };
 
-interface CartProviderProps {
+interface ProviderProps {
   children?: any;
 }
 
-export const Provider: FunctionComponent<CartProviderProps> = ({
+export const Provider: FunctionComponent<ProviderProps> = ({
   children,
 }) => {
   const [lang, setLang] = useState<Lang>('en_US');
@@ -34,8 +36,12 @@ export const Provider: FunctionComponent<CartProviderProps> = ({
     };
   }, []);
 
+  const userLangDefinitions = config.getConfig()?.langDefinitions;
+
+  const definitions = deepmerge(langDefinitions, userLangDefinitions ?? {});
+
   return (
-    <UIProvider lang={lang} langDefinitions={langDefinitions}>
+    <UIProvider lang={lang} langDefinitions={definitions}>
       {children}
     </UIProvider>
   );
